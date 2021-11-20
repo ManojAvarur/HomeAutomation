32399fb4b43942b09d036285e7774ea19c9f1902
ManojAvarur <manojavarur@gmail.com>
2021-11-20 17:24:47 +0530

    Code improvement

--- _Main_File/z-sump_class.ino
+++ _Main_File/z-sump_class.ino
@@ -1,36 +1,114 @@
 class sumpDetails{
     private:
+        short waterLevelInSump = -1;
+
         bool isWaterLevelAboveLow;
         bool isWaterLevelAboveMid;
 
+        bool lowCheckCompleted;
+        bool midCheckCompleted;
+
     public:
-        sumpDetails( int lowLevel, int midLevel ){
-            if( lowLevel != NULL ){
-                this->isWaterLevelAboveLow = ( lowLevel )? true : false;
-            }
-
-            if( midLevel != NULL ){
-                this->isWaterLevelAboveMid = ( midLevel )? true : false;
-            }
+        sumpDetails(){
+            lowCheckCompleted = false;
+            midCheckCompleted = false;
         }
 
-        void updateSumpDetails( bool updateFromLow = false ){
+        void updateSumpDetails( bool updateFromStart = false ){
 
-            if( digitalRead( WL_S_MID ) == 1 ){
-                pinMode( WL_S_MID, OUTPUT);
-                digitalWrite( WL_S_MID, LOW);
-                pinMode( WL_S_MID, INPUT);
-                if( digitalRead( WL_S_MID ) == 1 ){
-                    return true;
-                }
+            int tempWaterLevelInSump = waterLevelInSump;
+            if( tempWaterLevelInSump == -1 || tempWaterLevelInSump > 1 ){
+                tempWaterLevelInSump = 0;
             }
 
-            if( DEBUG_CODE ){
-                Serial.print("\nDEBUG CODE ENABLED : \n\tlow_water_level_in_tank called from ' " + from + " '\n\n");
-                Serial.print("\tLOW = " + String( digitalRead( WL_S_MID ) ) );
-                delay( DEBUG_DELAY_TIME );
+            switch( tempWaterLevelInSump ){
+                case 0 :
+                    lowDigitalPin( WL_S_LOW, true, true );
+                    if( digitalRead( WL_S_LOW ) == 1 ){
+
+                        lowDigitalPin( WL_S_LOW, true, true );
+                        if( digitalRead( WL_S_LOW ) == 1 ){
+                            Serial.println("\nWater Level above low in sump");
+                            waterLevelInSump = 1;
+                        } else {
+                            waterLevelInSump = -1;
+                        }
+
+                    } else {
+                        waterLevelInSump = -1;
+                    }
+
+                    break;
+
+                case 1:
+                    lowDigitalPin( WL_S_MID, true, true );
+                    if( digitalRead( WL_S_MID ) == 1 ){
+
+                        lowDigitalPin( WL_S_MID, true, true );
+                        if( digitalRead( WL_S_MID ) == 1 ){
+                            Serial.println("\nWater Level above mid in sump");
+                            waterLevelInSump = 2;
+                        } else {
+                            waterLevelInSump = -1;
+                        }
+
+                    } else {
+                        waterLevelInSump = -1;
+                    }
+
+                    break;
+
+                default:
+                    highDigitalPin( WL_S_LOW );
+                    highDigitalPin( WL_S_MID );
+                    break;
+
+
             }
-        }
+            // if( !( midCheckCompleted ) || !( isWaterLevelAboveMid ) || updateFromStart ){
+                
+            //     lowDigitalPin( WL_S_MID, true, true );
+            //     if( digitalRead( WL_S_MID ) == 1 ){
+
+            //         lowDigitalPin( WL_S_MID, true, true );
+            //         if( digitalRead( WL_S_MID ) == 1 ){
+            //             Serial.println("\nWater Level above mid in sump");
+            //             midCheckCompleted = true;
+            //             isWaterLevelAboveMid = true;
+            //             isWaterLevelAboveLow = true;
+            //         } else {
+            //             midCheckCompleted = false;
+            //             isWaterLevelAboveMid = false;
+            //             isWaterLevelAboveLow = false;
+            //         }
+            //     } else {
+            //         midCheckCompleted = false;
+            //         isWaterLevelAboveMid = false;
+            //         isWaterLevelAboveLow = false;
+            //     }
+            // }
+
+            // if( !( lowCheckCompleted ) || !( isWaterLevelAboveLow ) || updateFromStart ){
+
+            //     lowDigitalPin( WL_S_LOW, true, true );
+            //     if( digitalRead( WL_S_LOW ) == 1 ){
+
+            //         lowDigitalPin( WL_S_LOW, true, true );
+            //         if( digitalRead( WL_S_LOW ) == 1 ){
+            //             Serial.println("\nWater Level above low in sump");
+            //             lowCheckCompleted = true;
+            //         } else {
+            //             lowCheckCompleted = false;
+            //         }
+
+            //     } else {
+            //         lowCheckCompleted = false;
+            //     }
+            // } 
+
+            // highDigitalPin( WL_S_LOW );
+            // highDigitalPin( WL_S_MID );
+        } 
 
         int waterLevelInSump(){
             if( this->isWaterLevelAboveMid ){
