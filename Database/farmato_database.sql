886dc07b2505f1bd55f2735fa9be08e4b89c3bcc
ManojAvarur <manojavarur@gmail.com>
2021-12-23 02:41:19 +0530

    Few addition in frontend and ready with backend database

---
+++ Database/farmato_database.sql
@@ -0,0 +1,47 @@
+-- Check out json data type in mysql
+
+CREATE TABLE registered_node_mcu (
+  node_mcu_unique_id varchar(100) PRIMARY KEY,
+)
+
+CREATE TABLE user_login (
+  user_unique_id varchar(100) PRIMARY KEY,
+  user_node_mcu_reference VARCHAR(100) NOT NULL,
+  user_full_name char(50) NOT NULL,
+  user_password varchar(255) NOT NULL,
+  user_email_id varchar(100) NOT NULL,
+  user_is_admin boolean NOT NULL,
+  FOREIGN KEY (user_node_mcu_reference) REFERENCES registered_node_mcu(node_mcu_unique_id)
+)
+
+CREATE TABLE cookie_data (
+    cookie_user_unique_id varchar(100) NOT NULL,
+    token_id_1 varchar(50) NOT NULL,
+    token_id_2 varchar(50) NOT NULL,
+    FOREIGN KEY (cookie_user_unique_id) REFERENCES user_login(user_unique_id)
+)
+
+
+CREATE TABLE user_nodemcu_com (
+  unc_node_mcu_unique_id varchar(100) NOT NULL,
+  time_stamp datetime DEFAULT current_timestamp(),
+  sump_status tinyint NOT NULL,
+  tank_status tinyint NOT NULL,
+  motor_status boolean NOT NULL,
+  debug_log varchar(200),
+  pump_manual_overide_request BOOLEAN,
+  pump_manual_overide_status BOOLEAN,
+  FOREIGN KEY (unc_node_mcu_unique_id) REFERENCES registered_node_mcu(node_mcu_unique_id)
+)
+
+
+
+ALTER TABLE user_nodemcu_com
+    CHANGE time_stamp 
+        time_stamp TIMESTAMP NOT NULL
+                    DEFAULT CURRENT_TIMESTAMP
+                    ON UPDATE CURRENT_TIMESTAMP;
+
+
+
+
---
+++ _headers/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+    $connection = mysqli_connect( "localhost", "root", "", "farmato");
+
+    if ( !$connection ) {
+        die("Connection failed!");
+    }
+
+?>
