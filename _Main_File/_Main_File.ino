6c0f8c2bf204f18fbe975e4f4597f8d4670fa2e6
Manoj A M <manojavarur@gmail.com>
2021-12-31 02:36:43 +0530

    Completed sensor data update

--- _Main_File/_Main_File.ino
+++ _Main_File/_Main_File.ino
@@ -1,5 +1,6 @@
 // ------------------ Main Declaration --------------
-String URL = "http://iotproject.coolpage.biz/home_automation";
+// String URL = "http://iotproject.coolpage.biz/home_automation";
+String URL = "http://192.168.2.3/HomeAutomation-website/handel_request/nodemcu"
 String UNIQUE_ID = "4bb7abf6d3782611339eb6fe6326b96b6b4fca3d6f7e16f33367268806c5512c";
 
 // ------------------ Wi-Fi Settings ------------------
@@ -27,8 +28,9 @@ WiFiClient client;
 
 // ----------------- Json Libraies Settings ------------
 #include "z-ArduinoJson-v6.18.5.h"
-StaticJsonDocument<48> Json_result_responce;
-StaticJsonDocument<96> User_request_check;
+StaticJsonDocument<512> json_sensor_data_update;
+StaticJsonDocument<256> json_user_request;
+
 // StaticJsonDocument<192> DB_update;
 // StaticJsonDocument<768> TimeStamp;
 
@@ -37,10 +39,11 @@ bool DEBUG_CODE = true;
 int DEBUG_DELAY_TIME = 0; // 1 sec
 
 // ---------------- Execution Time Period -------------
-unsigned long PERIOD = 5 * 60 * 1000L; // Executes in every 5 mins
-int INITIAL_PUMP_LOAD_TIME = 5000; // 5 Sec
-unsigned long UPDATE_INTERVAL_TIME = 5 * 1000L;
-unsigned long TARGET_TIME = 0L; 
+// unsigned long PERIOD = 5 * 60 * 1000L; // Executes in every 5 mins
+// int INITIAL_PUMP_LOAD_TIME = 5000; // 5 Sec
+
+int USER_REQUEST_CHECK_INTERVAL = 30000;
+unsigned long USER_REQUEST_CHECK_INTERVAL_TARGET_TIME = 0L; 
 
 // ------------------ Class Instantiation -------------
 
@@ -51,9 +54,11 @@ Sump sumpObj = Sump( WL_S_LOW, WL_S_MID, DEBUG_CODE, DEBUG_DELAY_TIME );
 Tank tankObj = Tank( WL_T_LOW, WL_T_MID, WL_T_HIGH, DEBUG_CODE, DEBUG_DELAY_TIME );
 
 // ------------------ Extras --------------------------
+String DEBUG_LOG = "";
 
 // ---------------- Function Declaration -----------
 void water_pump();
 void motor_control( uint8_t value );
-void update_server();
+void update_server(  pump_manual_override_data = false );
+bool check_requests_from_server();
 void setup_wifi();
--- _Main_File/y-functions.ino
+++ _Main_File/y-functions.ino
@@ -2,16 +2,16 @@ void water_pump(){
     if( tankObj.waterLevelInTank() != 4 ){
         if ( sumpObj.waterLevelInSump() <= 0) {
             motor_control( MOTOR_OFF );
-            Serial.println("\nTank level low turning off the pump because low water in summp");
+            debug_log("\nTank level low turning off the pump because low water in summp");
         } else if( sumpObj.waterLevelInSump() == 3 ){
             motor_control( MOTOR_ON );
-            Serial.println("\nTank level low turning on the pump");
+            debug_log("\nTank level low turning on the pump");
         } else {
-            Serial.println("\nWater Level low in sump");
+            debug_log("\nWater Level low in sump");
         }
     } else {
         motor_control( MOTOR_OFF );
-        Serial.println("\nTank level high turning off the pump");
+        debug_log("\nTank level high turning off the pump");
     }
 
 }
@@ -29,15 +29,86 @@ void motor_control( uint8_t value ){
     //    digitalWrite( RELAY_1, value );
 }
 
-void update_server(){
+void update_server( pump_manual_override_data = false ){
     Serial.println("Inside update server");
     if( tankObj.isChanged || sumpObj.isChanged ){
-        Serial.prinln("\tUpdating server");
+        
+        json_sensor_data_update["nodemcu_id"] = UNIQUE_ID;
+        json_sensor_data_update["tank_status"] = tankObj.waterLevelInTank();
+        json_sensor_data_update["sump_status"] = sumpObj.waterLevelInTank();
+        json_sensor_data_update["motor_status"] = digitalRead( RELAY_1 );
+        json_sensor_data_update["debug_log"] = DEBUG_LOG;
+
+        int httpCode;
+        int repeatCount = 5; count = 0;
+        String jsonData;
+        serializeJson( json_sensor_data_update, jsonData );
+
+        do{
+            HTTPClient http;
+            http.begin(client, URL+"/update_sensor_data.php?data_ready=true&pump_manual_override_data=" + String( pump_manual_override_data ) );
+            http.addHeader("Content-Type", "application/json");
+
+            httpCode = http.GET( jsonData );
+
+            http.end();
+
+            Serial.println("Inside Send update server. Responce Code : " + String( httpCode ) );  
+        
+            if( httpCode != 200 ){
+                delay( 500 );
+                count += 1;
+            }
+
+        } while( httpCode != 200 && count <= repeatCount );
     }
 }
 
-void check_requests_from_server(){
-    Serial.println("Inside update server");
+bool check_requests_from_server(){
+    if( ( millis() - USER_REQUEST_CHECK_INTERVAL_TARGET_TIME ) >= USER_REQUEST_CHECK_INTERVAL ){
+
+        USER_REQUEST_CHECK_INTERVAL_TARGET_TIME += USER_REQUEST_CHECK_INTERVAL;
+        
+        int httpCode;
+        int repeatCount = 5; count = 0;
+        String result;
+        serializeJson( json_sensor_data_update, jsonData );
+
+        do{
+            HTTPClient http;
+            http.begin(client, URL+"/get_user_requests.php?" );
+            // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            httpCode = http.GET("nodemcut_id=" + UNIQUE_ID );
+
+            http.end();
+
+            Serial.println("Inside Send update server. Responce Code : " + String( httpCode ) );  
+        
+            if( httpCode == 404 ){
+                return false;
+            } else if( httpCode != 200 ){
+                delay( 500 );
+                count += 1;
+            } else {
+                result = http.getString();
+            }
+
+        } while( httpCode != 200 && count <= repeatCount );
+
+        if( count <= repeatCount ){
+            deserializeJson( json_user_request, result );
+            if( json_user_request["pump_manual_overide_request"].equals("1") ){
+                
+            }
+        }
+
+
+    }
+}
+
+void debug_log( String value ){
+    Serial.prinln( value );
+    DEBUG_LOG = value;
 }
 
 void setup_wifi(){
