037c0af154d0d303453882743ddb5e8333b669c7
Manoj A M <manojavarur@gmail.com>
2022-03-23 18:36:26 +0530

    Initial stage of webview(failure)

--- homeato/lib/main.dart
+++ homeato/lib/main.dart
@@ -1,2 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "HOMEATO",
+      home: Scaffold(
+          appBar: AppBar(
+          title: const Center(
+            child: Text("HOMEATO"),
+          ),
+        ),
+        body: const WebView(
+          initialUrl: "http://192.168.2.2/HomeAutomation/index.php",
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
